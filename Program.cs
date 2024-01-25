e1714896d33037bf72b1091298e71f901863e633
NIKKnak <nik88350759@yandex.ru>
2024-01-25 16:50:11 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,12 +1,79 @@
-﻿namespace Test
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Test
 {
     internal class Program
+
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
-            Console.WriteLine();
+
+
+
+
+
+
+
+
         }
+
+
+
+        public record Person
+        {
+            public string Name { get; set; }
+            public Person(string name) => Name = name;
+        }
+
+
+
+
+        class Account
+        {
+            public delegate void AccountHandler(string message);
+            public event AccountHandler? Notify;              // 1.Определение события
+            public Account(int sum) => Sum = sum;
+            public int Sum { get; private set; }
+            public void Put(int sum)
+            {
+                Sum += sum;
+                Notify?.Invoke($"На счет поступило: {sum}");   // 2.Вызов события 
+            }
+            public void Take(int sum)
+            {
+                if (Sum >= sum)
+                {
+                    Sum -= sum;
+                    Notify?.Invoke($"Со счета снято: {sum}");   // 2.Вызов события
+                }
+                else
+                {
+                    Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
+                }
+            }
+        }
+        static void Print1()
+        {
+            Console.WriteLine("Print1");
+        }
+        static void Print2()
+        {
+            Console.WriteLine("Print2");
+        }
+
+
+
+
     }
 }
---
+++ WorcWithNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class WorcWithNumbers
+    {
+
+        public void WorcNum()
+        {
+
+            string str = "asd dsfdfdsf dsfdsf f,d";
+
+            string[] end = str.Split(new char[] { ' ' , ','});
+            Console.WriteLine(end.Length);
+        }
+
+
+
+    }
+}
